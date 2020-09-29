198a94a7b4c4b76bf65e7b48eaf91ea3c63164bd
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-29 15:12:13 +0200

    add EmptyAlg

---
+++ K4TestFWCore/src/components/EmptyAlg.h
@@ -0,0 +1,28 @@
+#pragma once 
+
+// GAUDI
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/Property.h"
+
+
+class EmptyAlg : public GaudiAlgorithm {
+public:
+  explicit EmptyAlg(const std::string&, ISvcLocator*);
+  virtual ~EmptyAlg();
+  /**  Initialize.
+   *   @return status code
+   */
+  virtual StatusCode initialize() final;
+  /**  Execute.
+   *   @return status code
+   */
+  virtual StatusCode execute() final;
+  /**  Finalize.
+   *   @return status code
+   */
+  virtual StatusCode finalize() final;
+
+private:
+  // member variable 
+  int m_member = 0; 
+};
