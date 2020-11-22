51879e641a7ae155b444db4561bab172a9fcf6db
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-22 22:54:25 +0100

    add tfile test

--- k4TestFWCore/options/testAlgorithmWithTFile.py
+++ k4TestFWCore/options/testAlgorithmWithTFile.py
@@ -3,18 +3,18 @@ from Gaudi.Configuration import *
 from Configurables import k4DataSvc
 podioevent = k4DataSvc("EventDataSvc")
 
-from Configurables import CreateExampleEventData
-producer = CreateExampleEventData()
+from Configurables import TestAlgorithmWithTFile
+producer = TestAlgorithmWithTFile()
 
 from Configurables import PodioOutput
 out = PodioOutput("out")
-out.filename = "output_k4test_exampledata.root"
+out.filename = "output_k4test_algorithmWithTFile.root"
 out.outputCommands = ["keep *"]
 
 from Configurables import ApplicationMgr
 ApplicationMgr( TopAlg=[producer, out],
                 EvtSel="NONE",
-                EvtMax=1000000,
+                EvtMax=5000000,
                 ExtSvc=[podioevent],
                 OutputLevel=INFO,
                 StopOnSignal=True,
